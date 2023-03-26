28932c0e44bfd5db341eda409d0d85e51fca614d
Demezy <38487319+Demezy@users.noreply.github.com>
2023-03-26 18:47:45 +0300

    feat: add appbar

--- example/lib/features/main_page/presentation/main_page.dart
+++ example/lib/features/main_page/presentation/main_page.dart
@@ -10,56 +10,86 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ListView(
-          children: [
-            AnimatedHorizontalListView(
-              height: 200,
-              itemWidth: 300,
-              children: List.generate(
-                10,
-                (index) => Padding(
-                  key: Key('$index'),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: Color.lerp(Colors.amber, Colors.red, index / 10),
-                  ),
+        child: CustomScrollView(
+          slivers: [
+            const SliverAppBar(
+              pinned: true,
+              // kion logo
+              title: Text('Kion'),
+              actions: [
+                Padding(
+                  padding: EdgeInsets.only(right: 12.0),
+                  child: Icon(Icons.settings),
                 ),
-              ),
+                Icon(Icons.search),
+              ],
             ),
-            SectionHeading(
-              title: 'title',
-              onPressed: () {},
-            ),
-            AnimatedHorizontalListView(
-              height: 300,
-              itemWidth: 160,
-              children: List.generate(
-                10,
-                (index) => Padding(
-                  key: Key('$index'),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: Color.lerp(Colors.blue, Colors.purple, index / 10),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  AnimatedHorizontalListView(
+                    height: 200,
+                    itemWidth: 300,
+                    children: List.generate(
+                      10,
+                      (index) => Padding(
+                        key: Key('$index'),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Color.lerp(
+                            Colors.amber,
+                            Colors.red,
+                            index / 10,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-            SectionHeading(
-              title: 'title2',
-              onPressed: () {},
-            ),
-            AnimatedHorizontalListView(
-              height: 300,
-              itemWidth: 160,
-              children: List.generate(
-                10,
-                (index) => Padding(
-                  key: Key('$index'),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: Color.lerp(Colors.green, Colors.cyan, index / 10),
+                  SectionHeading(
+                    title: 'title',
+                    onPressed: () {},
                   ),
-                ),
+                  AnimatedHorizontalListView(
+                    height: 300,
+                    itemWidth: 160,
+                    children: List.generate(
+                      10,
+                      (index) => Padding(
+                        key: Key('$index'),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Color.lerp(
+                            Colors.blue,
+                            Colors.purple,
+                            index / 10,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SectionHeading(
+                    title: 'title2',
+                    onPressed: () {},
+                  ),
+                  AnimatedHorizontalListView(
+                    height: 300,
+                    itemWidth: 160,
+                    children: List.generate(
+                      10,
+                      (index) => Padding(
+                        key: Key('$index'),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Color.lerp(
+                            Colors.green,
+                            Colors.cyan,
+                            index / 10,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
