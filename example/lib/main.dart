5d037f6f65b6adab87455eb58eb743357355f60d
Demezy <38487319+Demezy@users.noreply.github.com>
2023-03-28 16:03:38 +0300

    add dio

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,22 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 
 import 'config/app_theme.dart';
+import 'config/constants.dart';
 import 'features/home_screen/presentation/home_screen.dart';
 
-void main() {
+void setup() async {
+  GetIt.I.registerLazySingleton<Dio>(
+    () => Dio(BaseOptions(baseUrl: Consts.baseUrl)),
+  );
+
+  await GetIt.I.allReady();
+}
+
+void main() async {
+  setup();
   runApp(const MyApp());
 }
 
@@ -12,13 +25,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      themeMode: ThemeMode.dark,
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      home: const HomeScreen(),
-      // home: const DetailsView(),
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        themeMode: ThemeMode.dark,
+        theme: AppTheme.light,
+        darkTheme: AppTheme.dark,
+        home: const HomeScreen(),
+        // home: const DetailsView(),
+      ),
     );
   }
 }
