27a47a5927d5b71f9b17ba7f8505274a97fbd39d
Demezy <38487319+Demezy@users.noreply.github.com>
2023-03-28 16:05:48 +0300

    add film providers to main screen

--- example/lib/features/main_page/presentation/main_page.dart
+++ example/lib/features/main_page/presentation/main_page.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../details/domain/film_details.dart';
+import '../data/film_repository.dart';
 import 'animated_horisontal_scroll_view.dart';
 
 part './section_heading.dart';
 part './see_all_button.dart';
+part './banner_film.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends ConsumerWidget {
   const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bannerFilms = ref.watch(bigBannerFilmsProvider);
+    final popularFilms = ref.watch(popularFilmsProvider);
+    final recommendedFilms = ref.watch(recommendedFilmsProvider);
+
     return Center(
       child: CustomScrollView(
         slivers: [
@@ -31,24 +39,49 @@ class MainPage extends StatelessWidget {
           SliverList(
             delegate: SliverChildListDelegate(
               [
+                // big baner films
                 AnimatedHorizontalListView(
                   height: 200,
                   itemWidth: 300,
-                  children: List.generate(
-                    10,
-                    (index) => Container(
-                      color: Color.lerp(
-                        Colors.amber,
-                        Colors.red,
-                        index / 10,
+                  children: bannerFilms.when(
+                    data: (films) => films.isEmpty
+                        ? const [
+                            Center(
+                              child: Text('empty'),
+                            )
+                          ]
+                        : films.map((film) => BannerFilm(film: film)).toList(),
+                    error: (_, __) => [
+                      Container(
+                        color: Colors.red[200],
+                        child: Center(
+                          child: Text(
+                            'error',
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge
+                                ?.copyWith(color: Colors.amber),
+                          ),
+                        ),
+                      )
+                    ],
+                    loading: () => List<Widget>.generate(
+                      10,
+                      (index) => Container(
+                        color: Color.lerp(
+                          Colors.amber,
+                          Colors.red,
+                          index / 10,
+                        ),
                       ),
                     ),
                   ),
                 ),
                 SectionHeading(
-                  title: 'title',
+                  title: 'Popular now',
                   onPressed: () {},
                 ),
+                //  popular now films
                 AnimatedHorizontalListView(
                   height: 300,
                   itemWidth: 160,
@@ -64,9 +97,10 @@ class MainPage extends StatelessWidget {
                   ),
                 ),
                 SectionHeading(
-                  title: 'title2',
+                  title: 'Recommend',
                   onPressed: () {},
                 ),
+                // recommended films
                 AnimatedHorizontalListView(
                   height: 300,
                   itemWidth: 160,
